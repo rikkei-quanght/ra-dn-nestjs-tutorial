3926ba9239ee3f30ca2787738c02e61d0e6919c9
rk_quang-ht <quanght@rikkeisoft.com>
2023-11-27 21:06:11 +0700

    session 09 | dotenv

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,14 +1,15 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist/interfaces/typeorm-options.interface';
+import 'dotenv/config';
 
 const options = {
     type: 'mysql',
-    host: '127.0.0.1',
-    port: 3306,
-    username: 'root',
-    password: 'password',
-    database: 'ra_module05',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     entities: [`${__dirname}/**/*.entity{.ts,.js}`],
     migrations: [`${__dirname}/../../db/migrations/*.{ts,js}`],
     autoLoadEntities: true,
